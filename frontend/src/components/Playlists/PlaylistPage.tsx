7e6dcdfcde4d17ae4763daf785f507aa36bde0ca
Harry <harrylin349@gmail.com>
2023-01-05 13:03:00 -0500

    add filter call on /playlists

--- frontend/src/components/Playlists/PlaylistPage.tsx
+++ frontend/src/components/Playlists/PlaylistPage.tsx
@@ -19,10 +19,39 @@ const dummy_items = [
   },
 ];
 
+const fetchPlaylists = async () => {
+  console.log(JSON.stringify({
+    playlistA: "1PhIh9as2SfCG1YweNqmNW",
+    playlistB: "6FW87hw1HeyqjSbB23Si0h",
+    token:
+      "BQBTjpn5bPXQV74CjyP-p09KouB2aGMvvtOhCDif4nUicafvu4DOj9WpaHaJ9_UkPPMvs-jS_ys_OqwgS5GuirYQawdMbafdjX3YT931B1aZLardknmP3H0TIehU78A1OVp-SL-Rn7fN0Jl_3hoi6sGHYVyfQnlTbRBkDu2YJCQxlQwgCSfydCxrhbLpV1-2EtQ",
+  }))
+  const response = await fetch(
+    "https://vhb0e07ykd.execute-api.us-east-2.amazonaws.com/filter",
+    {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        playlistA: "1PhIh9as2SfCG1YweNqmNW",
+        playlistB: "6FW87hw1HeyqjSbB23Si0h",
+        token:
+          "BQBTjpn5bPXQV74CjyP-p09KouB2aGMvvtOhCDif4nUicafvu4DOj9WpaHaJ9_UkPPMvs-jS_ys_OqwgS5GuirYQawdMbafdjX3YT931B1aZLardknmP3H0TIehU78A1OVp-SL-Rn7fN0Jl_3hoi6sGHYVyfQnlTbRBkDu2YJCQxlQwgCSfydCxrhbLpV1-2EtQ",
+      }),
+    }
+  );
+  const parsed = await response.json();
+  console.log("==parsed play", parsed);
+};
+
 export const PlaylistPage = () => {
   const { user } = useContext(AppContext);
   useEffect(() => {
     console.log("==user", user);
+    fetchPlaylists();
   }, []);
   return (
     <div className="flex flex-col w-full bg-gradient-to-b from-cyan-500 to-blue-500 items-center justify-center">
