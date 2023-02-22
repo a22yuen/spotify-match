7cf79204ccb07dd962e7b56b35b6786141c50ec4
Harry Lin <harrylin349@gmail.com>
2023-02-22 17:56:08 -0500

    refactor playlist code

--- frontend/src/components/Playlists/PlaylistPage.tsx
+++ frontend/src/components/Playlists/PlaylistPage.tsx
@@ -9,12 +9,17 @@ import {
 } from "../../api/spotify";
 import { UserHeader } from "../common/UserHeader";
 
+enum PlaylistType {
+  mine,
+  other,
+  results,
+}
 const NO_TOKEN = "NO_TOKEN";
 
 export const PlaylistPage = () => {
   const { user, setUser } = useContext(AppContext);
-  const [playlistA, setPlaylistA] = useState("");
-  const [playlistB, setPlaylistB] = useState("");
+  const [urlA, setUrlA] = useState("");
+  const [urlB, setUrlB] = useState("");
   const [playlistItemsA, setPlaylistItemsA] = useState([]);
   const [playlistItemsB, setPlaylistItemsB] = useState([]);
   const [playlistResults, setPlaylistResults] = useState([]);
@@ -33,30 +38,22 @@ export const PlaylistPage = () => {
     }
   }, []);
 
-  const searchPlaylist = async (playlistLetter: string, playlist: string) => {
+  const searchPlaylist = async (setPlaylistItems: any, playlist: string) => {
     try {
       const playlistId = playlist.split("/")[4]?.split("?")[0];
       const response = await fetchPlaylistItems(token, playlistId);
       console.log("==response", response);
-      if (playlistLetter === "A") {
-        setPlaylistItemsA(response);
-      } else if (playlistLetter === "B") {
-        setPlaylistItemsB(response);
-      }
+      setPlaylistItems(response);
     } catch (e) {
       console.log("Invalid search playlist URL", e);
       return;
     }
   };
 
-  const searchResults = async (
-    playlist1: string,
-    playlist2: string,
-    mode: string
-  ) => {
+  const searchResults = async (urlA: string, urlB: string, mode: string) => {
     try {
-      const playlistId1 = playlist1.split("/")[4]?.split("?")[0];
-      const playlistId2 = playlist2.split("/")[4]?.split("?")[0];
+      const playlistId1 = urlA.split("/")[4]?.split("?")[0];
+      const playlistId2 = urlB.split("/")[4]?.split("?")[0];
       const response = await fetchPlaylistResults(
         playlistId1,
         playlistId2,
@@ -72,66 +69,40 @@ export const PlaylistPage = () => {
     }
   };
 
-  const renderColumnA = () => {
+  const renderPlaylistCol = (
+    url: any,
+    setUrl: any,
+    playlistItems: any,
+    setPlaylistItems: any,
+    type: any
+  ) => {
+    const label =
+      type == PlaylistType.mine
+        ? "Your Playlist"
+        : type == PlaylistType.other
+        ? "Friend's Playlist"
+        : "Results";
     return (
       <div className="flex flex-col gap-2 w-full">
-        <div className="text-white py-2 text-lg"> Your Playlist </div>
-        <div className="flex flex-row gap-2">
+        <div className="text-white py-2 text-lg"> {label} </div>
+        <div
+          className={`flex flex-row gap-2 ${
+            type == PlaylistType.results ? "invisible" : "visible"
+          }`}
+        >
           <input
             className="rounded text-white bg-black px-2 py-1 border-2 border-white w-full"
             type="text"
             placeholder="Playlist Name"
-            value={playlistA}
+            value={url}
             onChange={(e) => {
-              setPlaylistA(e.target.value);
-              searchPlaylist("A", e.target.value);
+              setUrl(e.target.value);
+              searchPlaylist(setPlaylistItems, e.target.value);
             }}
           />
         </div>
         <div className="no-scrollbar h-96 overflow-y-auto rounded-lg">
-          {playlistItemsA && <PlaylistItems items={playlistItemsA} />}
-        </div>
-      </div>
-    );
-  };
-
-  const renderColumnB = () => {
-    return (
-      <div className="flex flex-col gap-2 w-full">
-        <div className="text-white py-2 text-lg"> Friend's Playlist </div>
-        <div className="flex flex-row gap-2">
-          <input
-            className="rounded text-white bg-black px-2 py-1 border-2 border-white w-full"
-            type="text"
-            placeholder="Playlist Name"
-            value={playlistB}
-            onChange={(e) => {
-              setPlaylistB(e.target.value);
-              searchPlaylist("B", e.target.value);
-            }}
-          />
-        </div>
-        <div className="no-scrollbar h-96 overflow-y-auto rounded-lg">
-          {playlistItemsB && <PlaylistItems items={playlistItemsB} />}
-        </div>
-      </div>
-    );
-  };
-
-  const renderResults = () => {
-    return (
-      <div className="flex flex-col gap-2 w-full">
-        <div className="text-white py-2 text-lg"> Resulting Playlist </div>
-        <div className="flex flex-row gap-2 invisible">
-          <input
-            className="rounded text-white bg-black px-2 py-1 border-2 border-white w-full"
-            type="text"
-            placeholder="Playlist Name"
-            value={playlistB}
-          />
-        </div>
-        <div className="no-scrollbar h-96 overflow-y-auto rounded-lg">
-          {playlistResults && <PlaylistItems items={playlistResults} />}
+          {playlistItems && <PlaylistItems items={playlistItems} />}
         </div>
       </div>
     );
@@ -157,15 +128,33 @@ export const PlaylistPage = () => {
         // test commit
       }
       <div className="flex flex-row w-3/4 px-32 gap-6 justify-between">
-        {renderColumnA()}
-        {renderColumnB()}
-        {renderResults()}
+        {renderPlaylistCol(
+          urlA,
+          setUrlA,
+          playlistItemsA,
+          setPlaylistItemsA,
+          PlaylistType.mine
+        )}
+        {renderPlaylistCol(
+          urlB,
+          setUrlB,
+          playlistItemsB,
+          setPlaylistItemsB,
+          PlaylistType.other
+        )}
+        {renderPlaylistCol(
+          "",
+          () => {},
+          playlistResults,
+          setPlaylistResults,
+          PlaylistType.results
+        )}
       </div>
-      <div className="my-6">
+      <div className="flex flex-row my-6 gap-2">
         <GreenButton
           onClick={() => {
             console.log("==click sim");
-            searchResults(playlistA, playlistB, "sim");
+            searchResults(urlA, urlB, "sim");
           }}
         >
           <p className="text-base font-semibold">Similarities</p>
@@ -173,7 +162,7 @@ export const PlaylistPage = () => {
         <GreenButton
           onClick={() => {
             console.log("==click sim");
-            searchResults(playlistA, playlistB, "diff");
+            searchResults(urlA, urlB, "diff");
           }}
         >
           <p className="text-base font-semibold">Differences</p>
